91076239d7faec7abaeb49dd5bf3f9fe0be10617
somairotevoli <somairotevoli@gmail.com>
2021-03-21 13:07:09 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .target(
            name: "SwiftUIWindowBinder",
-           path: "SwiftUIWindowBinder"), //path for target to look for sources
+           path: "Sources"), //path for target to look for sources
         
     ]
 )
