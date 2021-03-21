28deb14345340e35e050f4dde043b32c8d797848
somairotevoli <somairotevoli@gmail.com>
2021-03-21 13:38:03 -0500

    Update HelloWorldTests.swift

--- HelloWorldTests/HelloWorldTests.swift
+++ HelloWorldTests/HelloWorldTests.swift
@@ -1,6 +1,6 @@
 //
 //  HelloWorldTests.swift
-//  HelloWorldTests
+//  SwiftUIWindowBinder
 //
 //  Created by Steven Curtis on 23/06/2020.
 //  Copyright © 2020 Steven Curtis. All rights reserved.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import HelloWorld
 
-class HelloWorldTests: XCTestCase {
+class SwiftUIWindowBinder: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
